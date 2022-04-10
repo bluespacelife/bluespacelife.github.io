c2dc18a1ef1577dd908848e2f2871f488d90c560
xiyawei <yw.xi@gamehaus.com>
2022-04-10 13:30:01 +0800

    Site updated: 2022-04-10 13:30:01

--- js/bluespacelife.js
+++ js/bluespacelife.js
@@ -15,3 +15,14 @@ function goToSystem1() {
     $("#home-gate").hide();
     $("#system1").show();
 }
+
+/**
+ * 获取URL地址栏中的参数
+ * @param {参数名称} name 
+ * @returns 
+ */
+function getUrlParam(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+    var r = window.location.search.substr(1).match(reg); //匹配目标参数
+    if (r != null) return unescape(r[2]); return null; //返回参数值
+}
