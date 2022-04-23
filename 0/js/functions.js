99fa84a104753e0b0274ce741901927467570124
xiyawei <yw.xi@gamehaus.com>
2022-04-23 12:00:27 +0800

    Site updated: 2022-04-23 12:00:26

---
+++ 0/js/functions.js
@@ -0,0 +1,27 @@
+/**
+ * 回到主页
+ */
+function goHome() {
+    window.location.href = '/';
+}
+
+/**
+ * 跳转到文章主页
+ */
+function goToArticles() {
+    window.location.href = '/categories/article';
+}
+
+/**
+ * 跳转到笔记主页
+ */
+function goToNotes() {
+    window.location.href = '/categories/note';
+}
+
+/**
+ * 跳转到小提琴主页
+ */
+function goToViolin() {
+    window.location.href = '/categories/violin';
+}
