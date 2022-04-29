54535f3c178f651195b46827947d974d812df828
xiyawei <yw.xi@gamehaus.com>
2022-04-29 21:09:18 +0800

    Site updated: 2022-04-29 21:09:18

--- 0/pc/js/bluespacelife.js
+++ 0/pc/js/bluespacelife.js
@@ -25,3 +25,10 @@ function goToNotes() {
 function goToViolin() {
     window.location.href = '/categories/violin';
 }
+
+/**
+ * 跳转到星辰大海主页
+ */
+function goToBeauty() {
+    window.location.href = '/beauty/beauty.html';
+}
